f42b145b3508afd30b8ccf968acb05c086299349
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-01-15 19:40:34 +0500

    add .

--- js/app.js
+++ js/app.js
@@ -12,6 +12,15 @@ elForm['search-form__input'].addEventListener('input', () => {
 const update = (date) => {
     elRegionsList.innerHTML = ""
     date.forEach((country) => {
+        const {
+            name,
+            capital,
+            population,
+            continents,
+            currencies,
+            languages,
+            flags,
+        } = country;
         if (country.capital) {
             elRegionsList.innerHTML += `
         <li class="regions__item">
@@ -32,29 +41,41 @@ const update = (date) => {
                 <h4 class="regions__item-heading">Capital:</h4>
                 <p class="regions__item-text">${country.capital}</p>
             </div>
-        </div>
-        </li>`;
+            <div class="regions__item-content">
+                <h4 class="regions__item-heading">Currency:</h4>
+                <p class="regions__item-text">${currencies
+                    ? `${Object.values(currencies)[0].name
+                    } - ${Object.keys(currencies)}`
+                    : ""
+                }</p >
+            </div >
+            <div class="regions__item-content">
+                <h4 class="regions__item-heading">Common Languages::</h4>
+                <p class="regions__item-text">${Object.values(languages)}</p>
+            </div>
+        </div >
+        </li > `;
         } else {
             elRegionsList.innerHTML += `
-        <li class="regions__item">
+    < li class="regions__item" >
         <img class="regions__img" src="${country.flags.svg}" alt="">
-        <div class="regions__content">
-            <h3 class="regions__content-heading">
-                <a class="regions__content-link" href="main.html">${country.name.common}</a>
-            </h3>
-            <div class="regions__item-content">
-                <h4 class="regions__item-heading">Population:</h4>
-                <p class="regions__item-text">${country.population}</p>
+            <div class="regions__content">
+                <h3 class="regions__content-heading">
+                    <a class="regions__content-link" href="main.html">${country.name.common}</a>
+                </h3>
+                <div class="regions__item-content">
+                    <h4 class="regions__item-heading">Population:</h4>
+                    <p class="regions__item-text">${country.population}</p>
+                </div>
+                <div class="regions__item-content">
+                    <h4 class="regions__item-heading">Region:</h4>
+                    <p class="regions__item-text">${country.region}</p>
+                </div>
+                <div class="regions__item-content">
+                    <h4 class="regions__item-heading">Capital:</h4>
+                    <p class="regions__item-text">No capital</p>
+                </div>
             </div>
-            <div class="regions__item-content">
-                <h4 class="regions__item-heading">Region:</h4>
-                <p class="regions__item-text">${country.region}</p>
-            </div>
-            <div class="regions__item-content">
-                <h4 class="regions__item-heading">Capital:</h4>
-                <p class="regions__item-text">No capital</p>
-            </div>
-        </div>
         </li>`;
         }
     });
